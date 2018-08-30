775e1b3870ddb9767b04d90cb272e05cc4e0d38a
yaoting.gao <yaoting.gao@ifchange.com>
2018-08-30 20:26:50 +0800

    70-climbStairs

---
+++ 70-爬楼梯.c
@@ -0,0 +1,18 @@
+int climbStairs(int n) {
+    int *num = (int *)calloc(n, sizeof(int));
+    if (n == 1) {
+        return 1;
+    }
+    if (n == 2) {
+        return 2;
+    }
+    //直接递归写法会爆栈超时
+    // num = climbStairs(n-1) + climbStairs(n-2);
+    //return num;
+    num[0] = 1;
+    num[1] = 2;
+    for (int i=2;i<n;i++) {
+        num[i] = num[i-1] + num[i-2];
+    }
+    return num[n-1];
+}
